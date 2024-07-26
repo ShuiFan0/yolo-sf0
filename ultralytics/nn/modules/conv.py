899218091c946a18962c4ac51b6bab1f072fc392
氵帆 <595835443@qq.com>
2024-07-26 10:22:32 +0800

    ConvS添加对dropout模式的配置

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -38,13 +38,24 @@ def autopad(k, p=None, d=1):  # kernel, padding, dilation
 class ConvS(nn.Module):
     """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
 
-    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True, dropout=0):
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True, dropout=0, dropoutModel="Dropout2d"):
         """Initialize ConvS layer with given arguments including activation."""
         super().__init__()
         
         self.conv = Conv(c1=c1, c2=2*c2, k=k, s=s, p=p, g=g, d=d, act=act)
-        self.dropout = None if dropout == 0 else nn.Dropout2d(dropout, inplace= True)  # 添加 Dropout 层 
+        self.dropout = ConvS.getDroupout(dropout, dropoutModel)  # 添加 Dropout 层 
         
+    def getDroupout(dropout, dropoutModel):
+        if dropout == 0:
+            return None
+        
+        if dropout=="Dropout":
+            return nn.Dropout(dropout, inplace= True)
+        if dropout=="Dropout2d":
+            return nn.Dropout2d(dropout, inplace= True)
+
+        return None
+
     def forward(self, x):
         """Apply convolution, batch normalization and activation to input tensor."""
         cv = self.conv(x)
